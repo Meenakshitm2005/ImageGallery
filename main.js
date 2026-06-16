241c508e8cdaad540946f337871bb53261766a5d
Meenakshi TM <meenakshitm9@gmail.com>
2026-06-16 21:39:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
+function changeImage(imageName){
+    document.querySelector('.displayed-img').src =
+    `images/${imageName}`;
+}
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('.dark');
 const overlay = document.querySelector('.overlay');
-
-const images = [
-'pic1.jpg',
-'pic2.jpg',
-'pic3.jpg',
-'pic5.jpg',
-'pic6.jpg',
-'pic7.jpg'
-];
-
-images.forEach((image, index) => {
-
-```
-const thumb = document.createElement('img');
-
-thumb.src = `images/${image}`;
-
-if(index === 0){
-    thumb.classList.add('active');
-}
-
-thumb.addEventListener('click', () => {
-
-    displayedImage.src = `images/${image}`;
-
-    document
-        .querySelectorAll('.thumb-bar img')
-        .forEach(img => img.classList.remove('active'));
-
-    thumb.classList.add('active');
-});
-
-thumbBar.appendChild(thumb);
-```
 
 });
 
